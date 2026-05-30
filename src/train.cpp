fae2aec4cf7e9dbe402f75a26459933ae14bbaa3
KekshinMaks <kekshin.maksim@gmail.com>
2026-05-30 16:37:29 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -4,56 +4,56 @@
 Train::Train() : first(nullptr), countOp(0) {}
 
 Train::~Train() {
-    if (!first) return;
-    Car* current = first;
-    do {
-        Car* temp = current;
-        current = current->next;
-        delete temp;
-    } while (current != first);
+  if (!first) return;
+  Car* current = first;
+  do {
+    Car* temp = current;
+    current = current->next;
+    delete temp;
+  } while (current != first);
 }
 
 int Train::getOpCount() {
-    return countOp;
+  return countOp;
 }
 
 void Train::addCar(bool light) {
-    Car* newCar = new Car{light, nullptr, nullptr};
-    if (!first) {
-        first = newCar;
-        first->next = first;
-        first->prev = first;
-        return;
-    }
-    Car* last = first->prev;
-    newCar->next = first;
-    newCar->prev = last;
-    last->next = newCar;
-    first->prev = newCar;
+  Car* newCar = new Car{light, nullptr, nullptr};
+  if (!first) {
+    first = newCar;
+    first->next = first;
+    first->prev = first;
+    return;
+  }
+  Car* last = first->prev;
+  newCar->next = first;
+  newCar->prev = last;
+  last->next = newCar;
+  first->prev = newCar;
 }
 
 int Train::getLength() {
-    countOp = 0;
-    Car* current = first;
-    current->light = true;
-    
-    while (true) {
-        int steps = 0;
-        do {
-            current = current->next;
-            countOp++;
-            steps++;
-        } while (!current->light);
-        
-        current->light = false;
-        
-        for (int i = 0; i < steps; i++) {
-            current = current->prev;
-            countOp++;
-        }
-        
-        if (!current->light) {
-            return steps;
-        }
+  countOp = 0;
+  Car* current = first;
+  current->light = true;
+
+  while (true) {
+    int steps = 0;
+    do {
+      current = current->next;
+      countOp++;
+      steps++;
+    } while (!current->light);
+
+    current->light = false;
+
+    for (int i = 0; i < steps; i++) {
+      current = current->prev;
+      countOp++;
     }
+
+    if (!current->light) {
+      return steps;
+    }
+  }
 }
